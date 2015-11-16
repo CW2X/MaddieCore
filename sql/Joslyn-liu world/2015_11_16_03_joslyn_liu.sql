3539c2423f8437754e3fa4ebd3c4afd4fa18a089
Joslyn <sallpally@gmail.com>
2015-11-16 05:44:58 -0500

    Gordok Brew Barker (brewfest) was speaking orcish outside ironforge.

---
+++ sql/Joslyn-liu world/2015_11_16_03_joslyn_liu.sql
@@ -0,0 +1,4 @@
+-- Gordok Brew Barker (brewfest) was speaking orcish outside ironforge.
+
+replace into `creature_text`(`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`BroadcastTextId`,`TextRange`,`comment`) values (23685,0,0,'YOU WANT DRINK?!  WE GOT DRINK!',14,0,100,0,0,0,28559,0,'Gordok Brew Barker'),(23685,0,1,'HEY YOU!  DRINK OGRE BREWS!  MAKE YOU BIG AND STRONG!',14,0,100,0,0,0,28560,0,'Gordok Brew Barker'),(23685,0,2,'YOU TRY DA\' BEST, NOW TRY DA\' REST!  OGRE BREW!!',14,0,100,0,0,0,28561,0,'Gordok Brew Barker');
+
