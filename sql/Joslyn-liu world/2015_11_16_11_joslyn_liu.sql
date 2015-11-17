ea9c802451899252cf37d33bca7a152e8ab585fa
Joslyn <sallpally@gmail.com>
2015-11-16 23:14:45 -0500

    forgot to add objects as event only for brewfest.

---
+++ sql/Joslyn-liu world/2015_11_16_11_joslyn_liu.sql
@@ -0,0 +1,11 @@
+-- forgot to add objects as event only for brewfest.
+INSERT INTO `world`.`game_event_gameobject` 
+	(`eventEntry`, 
+	`guid`
+	)
+	VALUES
+	('24', 
+	'9365272'
+	), ('24', 
+	'9365271'
+	);
