7de74db42aa0b09a2e0e736bb89f46963d158993
Joslyn <sallpally@gmail.com>
2015-11-18 00:48:36 -0500

    Delete double spawned prowlers and young forest bears in elwynn forest.

---
+++ sql/Joslyn-liu world/2015_11_17_02_joslyn_liu.sql
@@ -0,0 +1,23 @@
+-- Delete double spawned prowlers and young forest bears in elwynn forest.
+
+DELETE FROM `world`.`creature` 
+	WHERE
+	`guid` = '81396' ;
+
+DELETE FROM `world`.`creature` 
+	WHERE
+	`guid` = '81399' ;
+DELETE FROM `world`.`creature` 
+	WHERE
+	`guid` = '81379' ;
+DELETE FROM `world`.`creature` 
+	WHERE
+	`guid` = '81431' ;
+	
+DELETE FROM `world`.`creature` 
+	WHERE
+	`guid` = '81402' ;
+
+DELETE FROM `world`.`creature` 
+	WHERE
+	`guid` = '81408' ;			
