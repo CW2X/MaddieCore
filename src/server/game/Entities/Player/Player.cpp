0e2471558197bc37e6f8e26cdf6cec48fac1c542
Joslyn <sallpally@gmail.com>
2015-11-21 09:01:21 -0500

    rogue energy regen fix, supposed to be a smooth restore, not chunk ticks.

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -2415,7 +2415,7 @@ void Player::Regenerate(Powers power)
         else
             m_powerFraction[power] = addvalue - integerValue;
     }
-    if (m_regenTimerCount >= 2000)
+	if (m_regenTimerCount >= (power == POWER_ENERGY ? 100 : 2000))
         SetPower(power, curValue);
     else
         UpdateUInt32Value(UNIT_FIELD_POWER1 + power, curValue);
