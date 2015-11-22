a0197ca77d6a1221c50d7ef43e3b4fc4bc4c7721
Joslyn <sallpally@gmail.com>
2015-11-22 05:25:15 -0500

    add cath square sign in dwarven dist, makes sence to be there.

---
+++ sql/Joslyn-liu world/2015_11_22_03_joslyn_liu.sql
@@ -0,0 +1,4 @@
+
+-- add cath square sign in dwarven dist, makes sence to be there.
+
+insert  into `gameobject`(`guid`,`id`,`map`,`zoneId`,`areaId`,`spawnMask`,`phaseMask`,`position_x`,`position_y`,`position_z`,`orientation`,`rotation0`,`rotation1`,`rotation2`,`rotation3`,`spawntimesecs`,`animprogress`,`state`,`VerifiedBuild`,`size`) values (9365379,2099,0,0,0,1,1,-8439.44,650.001,93.6326,0.721099,0,0,0.352788,0.935703,300,0,1,0,-1);
