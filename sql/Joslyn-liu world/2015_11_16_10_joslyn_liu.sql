8adc3beb26decfa4e9cbe3a074b86c37ba3080a0
Joslyn <sallpally@gmail.com>
2015-12-07 19:35:00 -0500

    Change sql to "update" instead of delete and insert.

--- sql/Joslyn-liu world/2015_11_16_10_joslyn_liu.sql
+++ sql/Joslyn-liu world/2015_11_16_10_joslyn_liu.sql
@@ -1,7 +1,3 @@
 -- Fix quest 9528 (A Cry For Help) Magwin fell though the dock, set InhabitType to 1.
-DELETE FROM `world`.`creature_template` 
-	WHERE
-	`entry` = '17312' ;
 
-INSERT  INTO `creature_template`(`entry`,`difficulty_entry_1`,`difficulty_entry_2`,`difficulty_entry_3`,`KillCredit1`,`KillCredit2`,`modelid1`,`modelid2`,`modelid3`,`modelid4`,`name`,`subname`,`IconName`,`gossip_menu_id`,`minlevel`,`maxlevel`,`exp`,`faction`,`npcflag`,`speed_walk`,`speed_run`,`scale`,`rank`,`dmgschool`,`BaseAttackTime`,`RangeAttackTime`,`BaseVariance`,`RangeVariance`,`unit_class`,`unit_flags`,`unit_flags2`,`dynamicflags`,`family`,`trainer_type`,`trainer_spell`,`trainer_class`,`trainer_race`,`type`,`type_flags`,`lootid`,`pickpocketloot`,`skinloot`,`resistance1`,`resistance2`,`resistance3`,`resistance4`,`resistance5`,`resistance6`,`spell1`,`spell2`,`spell3`,`spell4`,`spell5`,`spell6`,`spell7`,`spell8`,`PetSpellDataId`,`VehicleId`,`mingold`,`maxgold`,`AIName`,`MovementType`,`InhabitType`,`HoverHeight`,`HealthModifier`,`ManaModifier`,`ArmorModifier`,`DamageModifier`,`ExperienceModifier`,`RacialLeader`,`movementId`,`RegenHealth`,`mechanic_immune_mask`,`flags_extra`,`ScriptName`,`VerifiedBuild`) VALUES (17312,0,0,0,0,0,16976,0,0,0,'Magwin',NULL,NULL,0,10,10,0,231,2,1,1.14286,1,0,0,2000,2000,1,1,2,37632,2048,0,0,0,0,0,0,7,4096,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'',1,1,1,1,1,1,1,1,0,0,1,0,2,'npc_magwin',12340);
-
+UPDATE `creature_template`  SET `InhabitType` = '1'  WHERE `entry` = 'entry' ;
