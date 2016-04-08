6238a91f8803c3f16f9bbb337e88f47497c524cd
Joslyn <sallpally@gmail.com>
2016-04-07 20:36:40 -0400

    add more

--- sql/Joslyn-Maddie world/2016_04_07_04_joslyn_maddie.sql
+++ sql/Joslyn-Maddie world/2016_04_07_04_joslyn_maddie.sql
@@ -4,11 +4,20 @@ DELETE FROM `creature` 	WHERE `guid` = '56755' ;
 DELETE FROM `creature` 	WHERE `guid` = '56759' ;
 DELETE FROM `creature` 	WHERE `guid` = '56770' ;
 DELETE FROM `creature` 	WHERE `guid` = '56756' ;
+DELETE FROM `creature` 	WHERE `guid` = '56774' ;
+DELETE FROM `creature` 	WHERE `guid` = '55720' ;
+DELETE FROM `creature` 	WHERE `guid` = '56763' ;
 
 INSERT  INTO `creature`(`guid`,`id`,`map`,`zoneId`,`areaId`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`,`VerifiedBuild`,`size`) VALUES (15001531,15644,530,0,0,1,1,0,1,9830.5,-6678.63,2.99845,5.91414,300,0,0,83,104,0,0,0,0,0,-1); 
 INSERT  INTO `creature`(`guid`,`id`,`map`,`zoneId`,`areaId`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`,`VerifiedBuild`,`size`) VALUES (15001532,15644,530,0,0,1,1,0,1,9806.09,-6694.4,2.59231,3.64434,300,0,0,98,115,0,0,0,0,0,-1);
 INSERT  INTO `creature`(`guid`,`id`,`map`,`zoneId`,`areaId`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`,`VerifiedBuild`,`size`) VALUES (15001533,15644,530,0,0,1,1,0,1,9808.02,-6729,19.1896,1.75547,300,0,0,83,104,0,0,0,0,0,-1);
 INSERT  INTO `creature`(`guid`,`id`,`map`,`zoneId`,`areaId`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`,`VerifiedBuild`,`size`) VALUES (15001539,16162,530,0,0,1,1,0,1,8770.82,-6177.82,6.92961,0.654643,300,0,0,115,126,0,0,0,0,0,-1);
+INSERT  INTO `creature`(`guid`,`id`,`map`,`zoneId`,`areaId`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`,`VerifiedBuild`,`size`) VALUES (15001542,16162,530,0,0,1,1,0,1,8759.46,-6111.52,73.0136,1.51467,300,0,0,131,138,0,0,0,0,0,-1);
+INSERT  INTO `creature`(`guid`,`id`,`map`,`zoneId`,`areaId`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`,`VerifiedBuild`,`size`) VALUES (15001541,15645,530,0,0,1,1,0,1,8752.8,-6099.22,73.0369,4.97827,300,0,0,131,138,0,0,0,0,0,-1);
+INSERT  INTO `creature`(`guid`,`id`,`map`,`zoneId`,`areaId`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`,`VerifiedBuild`,`size`) VALUES (15001540,16162,530,0,0,1,1,0,1,8792.51,-6100.92,49.2612,3.21505,300,0,0,115,126,0,0,0,0,0,-1);
+
+
+
 
 
 
