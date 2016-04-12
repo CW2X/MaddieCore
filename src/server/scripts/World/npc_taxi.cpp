633b5e9224e6ca29b02d0ff27aae5aba2b2e8134
Joslyn <sallpally@gmail.com>
2016-04-11 21:20:55 -0400

    Core/Scripts/DB: Migrate Skyguard Handler Irena to SAI

--- src/server/scripts/World/npc_taxi.cpp
+++ src/server/scripts/World/npc_taxi.cpp
@@ -35,7 +35,6 @@ EndScriptData
 #define GOSSIP_BRAZEN           "I am ready to go to Durnholde Keep."
 #define GOSSIP_DABIREE1         "Fly me to Murketh and Shaadraz Gateways"
 #define GOSSIP_DABIREE2         "Fly me to Shatter Point"
-#define GOSSIP_IRENA            "Fly me to Skettis please"
 #define GOSSIP_VERONIA          "Fly me to Manaforge Coruu please"
 #define GOSSIP_DEESAK           "Fly me to Ogri'la please"
 #define GOSSIP_CRIMSONWING      "<Ride the gryphons to Survey Alcaz Island>"
@@ -76,10 +75,7 @@ public:
             break;
        
        
-        case 23413: // Blade's Edge Mountains - Skyguard Handler Irena
-            if (player->GetReputationRank(1031) >= REP_HONORED)
-                player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_IRENA, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 11);
-            break;
+       
        
         case 20162: // Netherstorm - Veronia
             //Behind Enemy Lines
@@ -136,10 +132,6 @@ public:
             break;
       
        
-        case GOSSIP_ACTION_INFO_DEF + 11:
-            player->CLOSE_GOSSIP_MENU();
-            player->CastSpell(player, 41278, true);               //TaxiPath 706
-            break;
        
         case GOSSIP_ACTION_INFO_DEF + 15:
             player->CLOSE_GOSSIP_MENU();
