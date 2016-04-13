4250d68ad1e9eaaa799f64ecbf444c224586388c
Joslyn <sallpally@gmail.com>
2016-04-13 01:24:26 -0400

    Scripts/Zangarmarsh:  Removes empty gossip_text that resulted in NPCs saying blank text to players after buffing them

--- src/server/scripts/Outland/zone_zangarmarsh.cpp
+++ src/server/scripts/Outland/zone_zangarmarsh.cpp
@@ -48,7 +48,7 @@ EndContentData */
 
 enum AshyenAndKeleth
 {
-    GOSSIP_REWARD_BLESS         = 0,
+    
 
     NPC_ASHYEN                  = 17900,
     NPC_KELETH                  = 17901,
@@ -117,7 +117,7 @@ public:
                 if (spell)
                 {
                     creature->CastSpell(player, spell, true);
-                    creature->AI()->Talk(GOSSIP_REWARD_BLESS);
+                   
                 }
             }
 
@@ -145,7 +145,7 @@ public:
                 if (spell)
                 {
                     creature->CastSpell(player, spell, true);
-                    creature->AI()->Talk(GOSSIP_REWARD_BLESS);
+                    
                 }
             }
             player->CLOSE_GOSSIP_MENU();
