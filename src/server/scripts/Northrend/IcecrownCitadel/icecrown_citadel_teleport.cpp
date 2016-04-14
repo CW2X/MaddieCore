d04a16e217b11e025172a47839b25e7103f83dd2
Joslyn <sallpally@gmail.com>
2016-04-13 23:28:06 -0400

    Scripts/ICC/Teleporter: moved gossips to db

--- src/server/scripts/Northrend/IcecrownCitadel/icecrown_citadel_teleport.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/icecrown_citadel_teleport.cpp
@@ -1,20 +1,21 @@
 /*
- * Copyright (C) 2008-2015 TrinityCore <http://www.trinitycore.org/>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+* Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
+*
+* This program is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the
+* Free Software Foundation; either version 2 of the License, or (at your
+* option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+* more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
+#include "GameObjectAI.h"
 #include "InstanceScript.h"
 #include "Player.h"
 #include "ScriptedGossip.h"
@@ -22,87 +23,85 @@
 #include "Spell.h"
 #include "icecrown_citadel.h"
 
-#define GOSSIP_SENDER_ICC_PORT 631
+static std::vector<uint32> const TeleportSpells =
+{
+	LIGHT_S_HAMMER_TELEPORT,        // 0
+	ORATORY_OF_THE_DAMNED_TELEPORT, // 1
+	0,                              // 2
+	RAMPART_OF_SKULLS_TELEPORT,     // 3
+	DEATHBRINGER_S_RISE_TELEPORT,   // 4
+	UPPER_SPIRE_TELEPORT,           // 5
+	SINDRAGOSA_S_LAIR_TELEPORT      // 6
+};
 
 class icecrown_citadel_teleport : public GameObjectScript
 {
-    public:
-        icecrown_citadel_teleport() : GameObjectScript("icecrown_citadel_teleport") { }
+	static_assert(DATA_UPPERSPIRE_TELE_ACT == 41, "icecrown_citadel.h DATA_UPPERSPIRE_TELE_ACT set to value != 41, gossip condition of the teleporters won't work as intended.");
 
-        bool OnGossipHello(Player* player, GameObject* go) override
-        {
-            if (InstanceScript* instance = go->GetInstanceScript())
-            {
-                if (instance->GetBossState(DATA_LORD_MARROWGAR) == DONE)
-                {
-                    if (go->GetEntry() != GO_SCOURGE_TRANSPORTER_LIGHTSHAMMER)
-                        player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport to Light's Hammer.", GOSSIP_SENDER_ICC_PORT, LIGHT_S_HAMMER_TELEPORT);
-                    if (go->GetEntry() != GO_SCOURGE_TRANSPORTER_ORATORY)
-                        player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport to the Oratory of the Damned.", GOSSIP_SENDER_ICC_PORT, ORATORY_OF_THE_DAMNED_TELEPORT);
-                }
-                if (instance->GetBossState(DATA_LADY_DEATHWHISPER) == DONE && go->GetEntry() != GO_SCOURGE_TRANSPORTER_RAMPART)
-                    player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport to the Rampart of Skulls.", GOSSIP_SENDER_ICC_PORT, RAMPART_OF_SKULLS_TELEPORT);
-                if (instance->GetBossState(DATA_ICECROWN_GUNSHIP_BATTLE) == DONE && go->GetEntry() != GO_SCOURGE_TRANSPORTER_DEATHBRINGER)
-                    player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport to the Deathbringer's Rise.", GOSSIP_SENDER_ICC_PORT, DEATHBRINGER_S_RISE_TELEPORT);
-                if (instance->GetData(DATA_UPPERSPIRE_TELE_ACT) == DONE && go->GetEntry() != GO_SCOURGE_TRANSPORTER_UPPERSPIRE)
-                    player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport to the Upper Spire.", GOSSIP_SENDER_ICC_PORT, UPPER_SPIRE_TELEPORT);
-                /// @todo Gauntlet event before Sindragosa
-                if (instance->GetBossState(DATA_VALITHRIA_DREAMWALKER) == DONE && go->GetEntry() != GO_SCOURGE_TRANSPORTER_SINDRAGOSA)
-                    player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport to Sindragosa's Lair", GOSSIP_SENDER_ICC_PORT, SINDRAGOSA_S_LAIR_TELEPORT);
-            }
+public:
+	icecrown_citadel_teleport() : GameObjectScript("icecrown_citadel_teleport") { }
 
-            player->SEND_GOSSIP_MENU(player->GetGossipTextId(go), go->GetGUID());
-            return true;
-        }
+	struct icecrown_citadel_teleportAI : public GameObjectAI
+	{
+		icecrown_citadel_teleportAI(GameObject* go) : GameObjectAI(go)
+		{
+		}
 
-        bool OnGossipSelect(Player* player, GameObject* /*go*/, uint32 sender, uint32 action) override
-        {
-            player->PlayerTalkClass->ClearMenus();
-            player->CLOSE_GOSSIP_MENU();
-            SpellInfo const* spell = sSpellMgr->GetSpellInfo(action);
-            if (!spell)
-                return false;
+		bool GossipSelect(Player* player, uint32 /*menuId*/, uint32 gossipListId) override
+		{
+			if (gossipListId >= TeleportSpells.size())
+				return false;
 
-            if (player->IsInCombat())
-            {
-                Spell::SendCastResult(player, spell, 0, SPELL_FAILED_AFFECTING_COMBAT);
-                return true;
-            }
+			player->PlayerTalkClass->ClearMenus();
+			player->CLOSE_GOSSIP_MENU();
+			SpellInfo const* spell = sSpellMgr->GetSpellInfo(TeleportSpells[gossipListId]);
+			if (!spell)
+				return false;
 
-            if (sender == GOSSIP_SENDER_ICC_PORT)
-                player->CastSpell(player, spell, true);
+			if (player->IsInCombat())
+			{
+				Spell::SendCastResult(player, spell, 0, SPELL_FAILED_AFFECTING_COMBAT);
+				return true;
+			}
 
-            return true;
-        }
+			player->CastSpell(player, spell, true);
+			return true;
+		}
+	};
+
+	GameObjectAI* GetAI(GameObject* go) const override
+	{
+		return GetIcecrownCitadelAI<icecrown_citadel_teleportAI>(go);
+	}
 };
 
 class at_frozen_throne_teleport : public AreaTriggerScript
 {
-    public:
-        at_frozen_throne_teleport() : AreaTriggerScript("at_frozen_throne_teleport") { }
+public:
+	at_frozen_throne_teleport() : AreaTriggerScript("at_frozen_throne_teleport") { }
 
-        bool OnTrigger(Player* player, AreaTriggerEntry const* /*areaTrigger*/) override
-        {
-            if (player->IsInCombat())
-            {
-                if (SpellInfo const* spell = sSpellMgr->GetSpellInfo(FROZEN_THRONE_TELEPORT))
-                    Spell::SendCastResult(player, spell, 0, SPELL_FAILED_AFFECTING_COMBAT);
-                return true;
-            }
+	bool OnTrigger(Player* player, AreaTriggerEntry const* /*areaTrigger*/) override
+	{
+		if (player->IsInCombat())
+		{
+			if (SpellInfo const* spell = sSpellMgr->GetSpellInfo(FROZEN_THRONE_TELEPORT))
+				Spell::SendCastResult(player, spell, 0, SPELL_FAILED_AFFECTING_COMBAT);
+			return true;
+		}
 
-            if (InstanceScript* instance = player->GetInstanceScript())
-                if (instance->GetBossState(DATA_PROFESSOR_PUTRICIDE) == DONE &&
-                    instance->GetBossState(DATA_BLOOD_QUEEN_LANA_THEL) == DONE &&
-                    instance->GetBossState(DATA_SINDRAGOSA) == DONE &&
-                    instance->GetBossState(DATA_THE_LICH_KING) != IN_PROGRESS)
-                    player->CastSpell(player, FROZEN_THRONE_TELEPORT, true);
+		if (InstanceScript* instance = player->GetInstanceScript())
+			if (instance->GetBossState(DATA_PROFESSOR_PUTRICIDE) == DONE &&
+				instance->GetBossState(DATA_BLOOD_QUEEN_LANA_THEL) == DONE &&
+				instance->GetBossState(DATA_SINDRAGOSA) == DONE &&
+				instance->GetBossState(DATA_THE_LICH_KING) != IN_PROGRESS)
+				player->CastSpell(player, FROZEN_THRONE_TELEPORT, true);
 
-            return true;
-        }
+		return true;
+	}
 };
 
 void AddSC_icecrown_citadel_teleport()
 {
-    new icecrown_citadel_teleport();
-    new at_frozen_throne_teleport();
+	new icecrown_citadel_teleport();
+	new at_frozen_throne_teleport();
 }
