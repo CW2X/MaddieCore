2f372d564de253cc3a70458d3129f3dfe22c01dc
Joslyn <sallpally@gmail.com>
2016-04-19 20:07:28 -0400

    Core/Gossips: added some sanity checks to prevent duplicate interaction

--- src/server/game/Handlers/SpellHandler.cpp
+++ src/server/game/Handlers/SpellHandler.cpp
@@ -269,10 +269,9 @@ void WorldSession::HandleGameObjectUseOpcode(WorldPacket& recvData)
 
     TC_LOG_DEBUG("network", "WORLD: Recvd CMSG_GAMEOBJ_USE Message [%s]", guid.ToString().c_str());
 
-    if (GameObject* obj = GetPlayer()->GetMap()->GetGameObject(guid))
+	if (GameObject* obj = GetPlayer()->GetGameObjectIfCanInteractWith(guid))
     {
-        if (!obj->IsWithinDistInMap(GetPlayer(), obj->GetInteractionDistance()))
-            return;
+       
 
         // ignore for remote control state
         if (GetPlayer()->m_mover != GetPlayer())
@@ -294,17 +293,15 @@ void WorldSession::HandleGameobjectReportUse(WorldPacket& recvPacket)
     if (_player->m_mover != _player)
         return;
 
-    GameObject* go = GetPlayer()->GetMap()->GetGameObject(guid);
-    if (!go)
-        return;
+	if (GameObject* go = GetPlayer()->GetGameObjectIfCanInteractWith(guid))
+	{
+		if (go->AI()->GossipHello(_player))
+			return;
 
-    if (!go->IsWithinDistInMap(_player, INTERACTION_DISTANCE))
-        return;
+		_player->UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_USE_GAMEOBJECT, go->GetEntry());
+	}
 
-    if (go->AI()->GossipHello(_player))
-        return;
-
-    _player->UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_USE_GAMEOBJECT, go->GetEntry());
+   
 }
 
 void WorldSession::HandleCastSpellOpcode(WorldPacket& recvPacket)
