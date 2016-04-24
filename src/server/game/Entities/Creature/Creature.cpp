928b8f4c3d95b7d6aa78d442f485a633c81a5ad1
Joslyn <sallpally@gmail.com>
2016-04-24 00:18:51 -0400

    Core/Creature: Prevent friendly vehicles from trying to attack their rider after dismounting.

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -591,6 +591,7 @@ void Creature::Update(uint32 diff)
                 IsAIEnabled = true;
                 if (!IsInEvadeMode() && LastCharmerGUID)
                     if (Unit* charmer = ObjectAccessor::GetUnit(*this, LastCharmerGUID))
+						if (CanStartAttack(charmer, true))
                         i_AI->AttackStart(charmer);
 
                 LastCharmerGUID.Clear();
