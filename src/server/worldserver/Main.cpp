b80c4d50988e7bcbbfc00036c6aeea5d2e04b602
Joslyn <sallpally@gmail.com>
2016-04-24 01:06:42 -0400

    Core/Database: Close the databases correctly when the DBUpdater fails * Also fixes a memory leak spotted by Aokromes

--- src/server/worldserver/Main.cpp
+++ src/server/worldserver/Main.cpp
@@ -456,9 +456,9 @@ bool StartDB()
     // Load databases
     DatabaseLoader loader("server.worldserver", DatabaseLoader::DATABASE_NONE);
     loader
-        .AddDatabase(WorldDatabase, "World")
-        .AddDatabase(CharacterDatabase, "Character")
-        .AddDatabase(LoginDatabase, "Login");
+		.AddDatabase(LoginDatabase, "Login")
+		.AddDatabase(CharacterDatabase, "Character")
+		.AddDatabase(WorldDatabase, "World");
 
     if (!loader.Load())
         return false;
