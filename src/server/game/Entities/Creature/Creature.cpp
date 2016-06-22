277c43e1d5c132c63a61cac27244023e612b5863
Joslyn <sallpally@gmail.com>
2016-06-22 03:36:24 -0400

    Core/Creatures: Fix dead creatures spawn position on restart
    Fix dead flying creatures ignoring being spawned below the floor in instances on server restart because of not taking into account vmap height.

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -1380,9 +1380,9 @@ bool Creature::LoadCreatureFromDB(ObjectGuid::LowType spawnId, Map* map, bool ad
         m_deathState = DEAD;
         if (CanFly())
         {
-            float tz = map->GetHeight(GetPhaseMask(), data->posX, data->posY, data->posZ, false);
-            if (data->posZ - tz > 0.1f)
-                Relocate(data->posX, data->posY, tz);
+			float tz = map->GetHeight(GetPhaseMask(), data->posX, data->posY, data->posZ, true, MAX_FALL_DISTANCE);
+			if (data->posZ - tz > 0.1f && Trinity::IsValidMapCoord(tz))
+				Relocate(data->posX, data->posY, tz);
         }
     }
 
