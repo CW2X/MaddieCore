3a416ef799bb0b27746f373255ca7acbc4005a0c
Joslyn <sallpally@gmail.com>
2016-07-05 15:29:47 -0400

    Add AC2
    Aggressive anticheat, blocks Hitchhiker other than tracking.

--- src/server/game/Entities/Unit/Unit.h
+++ src/server/game/Entities/Unit/Unit.h
@@ -2151,6 +2151,7 @@ class Unit : public WorldObject
     protected:
         explicit Unit (bool isWorldObject);
 
+		GameObject* m_temp_transport;
         void BuildValuesUpdate(uint8 updatetype, ByteBuffer* data, Player* target) const override;
 
         UnitAI* i_AI, *i_disabledAI;
--- src/server/game/World/World.h
+++ src/server/game/World/World.h
@@ -820,6 +820,15 @@ class World
         static int32 GetVisibilityNotifyPeriodInInstances() { return m_visibility_notify_periodInInstances;  }
         static int32 GetVisibilityNotifyPeriodInBGArenas()  { return m_visibility_notify_periodInBGArenas;   }
 
+		// movement anticheat
+		static bool GetEnableMvAnticheat()       { return m_EnableMvAnticheat; }
+		static uint32 GetTeleportToPlaneAlarms() { return m_TeleportToPlaneAlarms; }
+		static uint32 GetMistimingDelta()        { return m_MistimingDelta; }
+		static uint32 GetMistimingAlarms()       { return m_MistimingAlarms; }
+		static uint32 GetTeleportToPlanePenalty() { return m_TeleportToPlanePenalty; }
+		static uint32 GetMovementPenalty()        { return m_MovementPenalty; }
+		// end movement anticheat
+
         void ProcessCliCommands();
         void QueueCliCommand(CliCommandHolder* commandHolder) { cliCmdQueue.add(commandHolder); }
 
@@ -933,6 +942,17 @@ class World
         static int32 m_visibility_notify_periodOnContinents;
         static int32 m_visibility_notify_periodInInstances;
         static int32 m_visibility_notify_periodInBGArenas;
+
+		// movement anticheat enable flag
+		public:
+		static bool m_EnableMvAnticheat;
+		static uint32 m_TeleportToPlaneAlarms;
+		static uint32 m_TeleportToPlanePenalty;
+		static uint32 m_MovementPenalty;
+		static uint32 m_MistimingDelta;
+		static uint32 m_MistimingAlarms;
+		private:
+		//END movement anticheat enable flag
 
         // CLI command holder to be thread safe
         LockedQueue<CliCommandHolder*> cliCmdQueue;
